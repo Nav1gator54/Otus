4aca4d89f07c190267e01b9675425d332cc63d47
Vladimir <Bulldog777@bk.ru>
2021-09-20 21:07:20 +0700

    Second Commit

--- hello.go
+++ hello.go
@@ -2,23 +2,8 @@ package main
 
 import (
 	"fmt"
-	//. "github.com/beevik/ntp"
-	//"os"
 )
 
 func main() {
 	fmt.Print("Enter host: ")
-
-	//var host string
-	//fmt.Scanln(&host)
-	//
-	//time, err := Time(host)
-	//
-	//if err != nil {
-	//	fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	//	os.Exit(1)
-	//}
-	//
-	//fmt.Println(time)
-
 }
