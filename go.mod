6b45c25e0e89a1b5296820f293aaea0c0038a2f6
Vladimir <Bulldog777@bk.ru>
2021-09-20 21:49:50 +0700

    Done

---
+++ go.mod
@@ -0,0 +1,8 @@
+module work
+
+go 1.15
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
+)
--- hello.go
+++ hello.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Print("Enter host: ")
+	response, err := ntp.Query("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Printf("Error from server: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(response.Time.Add(7 * time.Hour))
 }
